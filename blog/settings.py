8554ffe6df0b585ad5cf760e7dcb72a1308516a4
Nathalie <nbleijie@gmail.com>
2020-03-23 16:23:07 +0000

    manually adjusted pillow version needed for python 2.7 due to travis error

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-ci-test-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','blog-ci-test-app.herokuapp.com']
 
 
 # Application definition
